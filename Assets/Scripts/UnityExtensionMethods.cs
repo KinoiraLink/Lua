2a675f6fafb2845526243fc84b6271a36b892f53
KinoiraLink <summerkino@outlook.com>
2024-04-20 20:41:21 +0800

    IO

---
+++ Assets/Scripts/UnityExtensionMethods.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using XLua;
+
+namespace EmmyLua
+{
+    [LuaCallCSharp]
+    public static class UnityExtensionMethods
+    {
+        
+        public static string GetTextAsset(string path)
+        {
+            
+            
+            return Resources.Load<TextAsset>(path).text;
+            
+        }
+        
+        public static Button GetButton(this GameObject go)
+        {
+            return go.GetComponent<Button>();
+        }
+
+        public static string ReadFileText(string path)
+        {
+            string rootPath = Application.dataPath;
+            string content = File.ReadAllText(Path.Combine(rootPath,"Resources",path));
+            return content;
+        }
+    }
+}
